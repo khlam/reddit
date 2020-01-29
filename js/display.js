930603a17095957da8f8087948ce9d6ab089b28c
khlam <4841220+khlam@users.noreply.github.com>
2020-01-28 16:23:53 -0800

    organization

--- js/display.js
+++ js/display.js
@@ -1,13 +1,39 @@
+function stylePost(post) {
+    return `
+        <td>${post['upvotes']}</td>
+        <td>.thumbnail</td>
+        <td>
+                <div class="row">
+                    <div class="col-9">
+                        <a href="${post['postURL']}">${post['title']}</a>
+                    </div>
+                    <div class="col-2">
+                    (<a href="${post['postDomain']}">${post['postDomain']}</a>)
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                    submitted by <a href="https://old.reddit.com/u/${post['author']}">${post['author']}</a> to <a href="https://old.reddit.com/r/${post['subreddit']}">r/${post['subreddit']}</a>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-6">
+                    <a href="${post['commentsURL']}">${post['commentsNumber']} comments</a>
+                    </div>
+                </div>
+        </td>
+
+        `
+}
+
 function createTable(allPosts) {
     console.log(allPosts)
     var result = "<table border=1>";
     allPosts.forEach(post => {
         result += "<tr>"
             //console.log(post)
-            Object.keys(post).forEach(function(key) {
-                //console.log(key, obj[key]);
-                result += "<td>"+post[key]+"</td>";
-            });
+            result += stylePost(post)
 
         result += "</tr>"
     });
